04288354e1b0d218399d765b46eab9e9f8ce0508
shitomi maiko <s.t.m3m.i.k@gmail.com>
2020-01-30 23:32:57 +0900

    Wordpressの設定

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>トップページ</title>
+    
+    <!-- ディスプレイ -->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    
+    <!--  CSS  -->
+    <link rel="stylesheet" href="./assets/style.css">
+    
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <div class="header-logo">header</div>
+        <nav class="header-navi">
+          <ul>
+            <li><a href="#">TOP</a></li>
+            <li><a href="#">BLOG</a></li>
+            <li><a href="#">SHOP</a></li>
+            <li><a href="#">PLIVACY POLICY</a></li>
+            <li><a href="#">CONTACT</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div class="hero">
+        <h1>TOP</h1>
+        <div class="image">
+
+        </div>
+      </div>
+      <div class="announce">
+        <h2>announce</h2>
+        <div class="announce-content">
+          <p class="date">2020/02/20</p>
+          <h3>announce-title</h3>
+          <p class="texrt">announce text.announce text.announce text.announce text.announce text.</p>
+        </div>
+        <div class="announce-content">
+          <p class="date">2020/02/20</p>
+          <h3>announce-title</h3>
+          <p class="texrt">announce text.announce text.announce text.announce text.announce text.</p>
+          <a class="post-link" href="#">post-link</a>
+        </div>
+      </div>
+      <article>
+        <div class="top-one">
+
+        </div>
+        <div class="top-two">
+
+        </div>
+      </article>
+    </div>
+    <footer>
+      <div class="container">
+        <nav class="footer-navi">
+            <ul>
+              <li><a href="#">TOP</a></li>
+              <li><a href="#">BLOG</a></li>
+              <li><a href="#">SHOP</a></li>
+              <li><a href="#">PLIVACY POLICY</a></li>
+              <li><a href="#">CONTACT</a></li>
+            </ul>
+          </nav>
+          <div class="footer-logo">footer</div>
+      </div>
+    </footer>
+  </body>
+  </html>
